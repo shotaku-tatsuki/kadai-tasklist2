06c3e1666473af920ff4ea940986f4c73d02318c
shotaku.tatsuki <jongtek-7-17@yahoo.co.jp>
2020-04-18 21:09:19 +0000

    certification

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index]
+  before_action :require_user_logged_in
+  before_action :correct_user, except: [:index]
+  
   def index
-    @tasks = Task.all
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
   
   def show
@@ -13,8 +15,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスクが正常に保存されました'
       redirect_to @task
@@ -40,7 +41,6 @@ class TasksController < ApplicationController
   
   def destroy
     @task.destroy
-    
     flash[:success] = 'タスクは正常に削除されました'
     redirect_to tasks_url
   end
@@ -54,5 +54,12 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+    def correct_user
+      @task = current_user.tasks.find_by(id: params[:id])
+      unless @task
+        redirect_to root_url
+      end
+    end
   
 end
